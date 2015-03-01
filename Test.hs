2c53673b4245fc0bef68d39d7e8c625d7937e128
Oliver Charles <ollie@ocharles.org.uk>
2015-03-01 19:43:19 +0000

    Update controlled component test

--- Test.hs
+++ Test.hs
@@ -6,15 +6,25 @@ module Main where
 import Francium.Component
 import Francium.Components.Form.Input
 import Francium
+import Francium.Hooks
 import VirtualDom
 import Data.Char
 import GHCJS.Foreign
 
 main :: IO ()
 main =
-  react (mdo input <- construct (Input value)
+  react (mdo input <- construct (Input v)
+             (clickHook,onClick) <- newClickHook
+             let v =
+                   fmap (toJSString . show)
+                        (accumB 0 (fmap (const (+ 1)) onClick))
              let value =
                    stepper ""
                            (fmap (toJSString . map toUpper . fromJSString)
                                  (inputChanged (outputs input)))
-             return (render input))
+             return (fmap (\a ->
+                             into div_
+                                  [a
+                                  ,applyHooks clickHook
+                                              (into button_ ["Inc"])])
+                          (render input)))
